772b8ae091c5e414fbd19745c645a5c37ac5b4ba
Yn <30512781+akayn@users.noreply.github.com>
2017-09-12 22:03:56 -0700

    Update HEVD-IntegerOverFlowX86Win7.c

--- HEVD-Vanilla-Bug-Class's/HEVD-IntegerOverFlowX86Win7.c
+++ HEVD-Vanilla-Bug-Class's/HEVD-IntegerOverFlowX86Win7.c
@@ -1,3 +1,82 @@
+
+/*
+
+NTSTATUS TriggerIntegerOverflow(IN PVOID UserBuffer, IN SIZE_T Size) {
+    ULONG Count = 0;
+    NTSTATUS Status = STATUS_SUCCESS;
+    ULONG BufferTerminator = 0xBAD0B0B0;
+    ULONG KernelBuffer[BUFFER_SIZE] = {0};
+    SIZE_T TerminatorSize = sizeof(BufferTerminator);
+ 
+    PAGED_CODE();
+ 
+    __try {
+        // Verify if the buffer resides in user mode
+        ProbeForRead(UserBuffer, sizeof(KernelBuffer), (ULONG)__alignof(KernelBuffer));
+ 
+        DbgPrint("[+] UserBuffer: 0x%p\n", UserBuffer);
+        DbgPrint("[+] UserBuffer Size: 0x%X\n", Size);
+        DbgPrint("[+] KernelBuffer: 0x%p\n", &KernelBuffer);
+        DbgPrint("[+] KernelBuffer Size: 0x%X\n", sizeof(KernelBuffer));
+ 
+#ifdef SECURE
+        // Secure Note: This is secure because the developer is not doing any arithmetic
+        // on the user supplied value. Instead, the developer is subtracting the size of
+        // ULONG i.e. 4 on x86 from the size of KernelBuffer. Hence, integer overflow will
+        // not occur and this check will not fail
+        if (Size > (sizeof(KernelBuffer) - TerminatorSize)) {
+            DbgPrint("[-] Invalid UserBuffer Size: 0x%X\n", Size);
+ 
+            Status = STATUS_INVALID_BUFFER_SIZE;
+            return Status;
+        }
+#else
+        DbgPrint("[+] Triggering Integer Overflow\n");
+ 
+        // Vulnerability Note: This is a vanilla Integer Overflow vulnerability because if
+        // 'Size' is 0xFFFFFFFF and we do an addition with size of ULONG i.e. 4 on x86, the
+        // integer will wrap down and will finally cause this check to fail
+        if ((Size + TerminatorSize) > sizeof(KernelBuffer)) {
+            DbgPrint("[-] Invalid UserBuffer Size: 0x%X\n", Size);
+ 
+            Status = STATUS_INVALID_BUFFER_SIZE;
+            return Status;
+        }
+#endif
+ 
+        // Perform the copy operation
+        while (Count < (Size / sizeof(ULONG))) {
+            if (*(PULONG)UserBuffer != BufferTerminator) {
+                KernelBuffer[Count] = *(PULONG)UserBuffer;
+                UserBuffer = (PULONG)UserBuffer + 1;
+                Count++;
+            }
+            else {
+                break;
+            }
+        }
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        Status = GetExceptionCode();
+        DbgPrint("[-] Exception Code: 0x%X\n", Status);
+    }
+ 
+    return Status;
+}
+
+'''
+Obvious bug is obvious, the terminator size is 4 bytes so if we supply DeviceIoControl with a buffer size which
+is between 0xfffffffc and 0xffffffff the driver will add 4 to the integer causing the value to loop round
+on itself and pass the check.
+'''
+
+what he say'd + we can control the callback giving us control over the execution flow.
+
+
+*/
+
+
+
 #include <Windows.h>
 #include <stdio.h>
 #include <winioctl.h>
