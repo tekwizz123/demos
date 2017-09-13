28b00ad2127ee37fb5436c0f251dde84d113c1dd
Yn <30512781+akayn@users.noreply.github.com>
2017-09-12 22:04:37 -0700

    Update HEVD-ArbitraryOverwritex86win7.c

--- HEVD-Vanilla-Bug-Class's/HEVD-ArbitraryOverwritex86win7.c
+++ HEVD-Vanilla-Bug-Class's/HEVD-ArbitraryOverwritex86win7.c
@@ -55,7 +55,6 @@ The issue here is that the driver does not validate that the location
 of the destination pointer is in userland,
 because of this we can overwrite an arbitrary Kernel address (4-bytes) with and arbitrary value (4-bytes).
 '''
-B33F
 
 <-----------------
 To Put it simply The driver lets us write arbitrary data to an arbitrary location.
