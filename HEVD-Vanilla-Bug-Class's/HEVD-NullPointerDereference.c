66fd87a702e53f8919957244cc0d5a5b9e8a06f9
Yn <30512781+akayn@users.noreply.github.com>
2017-09-12 21:40:40 -0700

    Update HEVD-NullPointerDereference.c

--- HEVD-Vanilla-Bug-Class's/HEVD-NullPointerDereference.c
+++ HEVD-Vanilla-Bug-Class's/HEVD-NullPointerDereference.c
@@ -1,3 +1,102 @@
+
+/*
+NTSTATUS TriggerNullPointerDereference(IN PVOID UserBuffer) {
+    ULONG UserValue = 0;
+    ULONG MagicValue = 0xBAD0B0B0;
+    NTSTATUS Status = STATUS_SUCCESS;
+    PNULL_POINTER_DEREFERENCE NullPointerDereference = NULL;
+ 
+    PAGED_CODE();
+ 
+    __try {
+        // Verify if the buffer resides in user mode
+        ProbeForRead(UserBuffer,
+                     sizeof(NULL_POINTER_DEREFERENCE),
+                     (ULONG)__alignof(NULL_POINTER_DEREFERENCE));
+ 
+        // Allocate Pool chunk
+        NullPointerDereference = (PNULL_POINTER_DEREFERENCE)
+                                  ExAllocatePoolWithTag(NonPagedPool,
+                                                        sizeof(NULL_POINTER_DEREFERENCE),
+                                                        (ULONG)POOL_TAG);
+ 
+        if (!NullPointerDereference) {
+            // Unable to allocate Pool chunk
+            DbgPrint("[-] Unable to allocate Pool chunk\n");
+ 
+            Status = STATUS_NO_MEMORY;
+            return Status;
+        }
+        else {
+            DbgPrint("[+] Pool Tag: %s\n", STRINGIFY(POOL_TAG));
+            DbgPrint("[+] Pool Type: %s\n", STRINGIFY(NonPagedPool));
+            DbgPrint("[+] Pool Size: 0x%X\n", sizeof(NULL_POINTER_DEREFERENCE));
+            DbgPrint("[+] Pool Chunk: 0x%p\n", NullPointerDereference);
+        }
+ 
+        // Get the value from user mode
+        UserValue = *(PULONG)UserBuffer;
+ 
+        DbgPrint("[+] UserValue: 0x%p\n", UserValue);
+        DbgPrint("[+] NullPointerDereference: 0x%p\n", NullPointerDereference);
+ 
+        // Validate the magic value
+        if (UserValue == MagicValue) {
+            NullPointerDereference->Value = UserValue;
+            NullPointerDereference->Callback = &NullPointerDereferenceObjectCallback;
+ 
+            DbgPrint("[+] NullPointerDereference->Value: 0x%p\n", NullPointerDereference->Value);
+            DbgPrint("[+] NullPointerDereference->Callback: 0x%p\n", NullPointerDereference->Callback);
+        }
+        else {
+            DbgPrint("[+] Freeing NullPointerDereference Object\n");
+            DbgPrint("[+] Pool Tag: %s\n", STRINGIFY(POOL_TAG));
+            DbgPrint("[+] Pool Chunk: 0x%p\n", NullPointerDereference);
+ 
+            // Free the allocated Pool chunk
+            ExFreePoolWithTag((PVOID)NullPointerDereference, (ULONG)POOL_TAG);
+ 
+            // Set to NULL to avoid dangling pointer
+            NullPointerDereference = NULL;
+        }
+ 
+#ifdef SECURE
+        // Secure Note: This is secure because the developer is checking if
+        // 'NullPointerDereference' is not NULL before calling the callback function
+        if (NullPointerDereference) {
+            NullPointerDereference->Callback();
+        }
+#else
+        DbgPrint("[+] Triggering Null Pointer Dereference\n");
+ 
+        // Vulnerability Note: This is a vanilla Null Pointer Dereference vulnerability
+        // because the developer is not validating if 'NullPointerDereference' is NULL
+        // before calling the callback function
+        NullPointerDereference->Callback();
+#endif
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        Status = GetExceptionCode();
+        DbgPrint("[-] Exception Code: 0x%X\n", Status);
+    }
+ 
+    return Status;
+}
+ 
+Ok, so we have a check on a magic value, if it succeeds we print the value and the callback function
+(this is normal execution flow). If the check fails we free the pool tag and null the pointer.
+Up to there there is no issue but then, in the vulnerable version, the driver simply calls the callback function without
+checking if it was previously nulled!
+
+<-----------------
+To Put it simply the driver calls the callback
+checking if it was previously nulled, on win-7 you can map the null page, again giving the attaker
+control over the kernel execution flow.
+---------------->
+
+*/
+
+
 #include "stdafx.h"
 #include <Windows.h>
 #include <string.h>
